78285c5963db0da24969f3d21f7e1633e5a902e2
cavok-ky <cavok.ky@gmail.com>
2024-04-05 18:14:13 +0900

    Edit cicd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './flower.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to see the magic of spring.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd6
+          Spring Spring Spring
         </a>
       </header>
     </div>
